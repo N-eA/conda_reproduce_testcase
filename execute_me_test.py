921533568cfbb925ef5971d82f89341fec946f71
N-eA <nea@newworldelectric.com>
2020-07-17 13:30:17 +0200

    execute_me_test.py now commented and passing

--- execute_me_test.py
+++ execute_me_test.py
@@ -6,22 +6,25 @@ import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
 
-#print(f'Scipy call: {csr_matrix([[1,0,2],[0,3,0]])}')
-
-#print(f'Numpy call: {np.zeros(5)}')
-
-#print(f'Pandas call: {pd.Series(["a","b","c","a"], dtype="category")}')
-
-#print(f'Scipy call: {csr_matrix([[1,0,2],[0,3,0]])}')
-
-#print(f'Numpy call: {np.zeros(5)}')
-
-#print(f'Pandas call: {pd.Series(["a","b","c","a"], dtype="category")}')
-
+"""
+This testscript creates some vanilla test examples by using the imported libraries, the goal is to
+see all of the tests passing when migrating/recreating conda on different distributions
+"""
 
 def test_scipy():
-    values =[[1,0,2],[0,3,0]] 
+    values = np.array([[1,0,2],[0,3,0]])
+    expected_val = values
+    ret_val = csr_matrix(values).toarray()
+    # Nested arrays thus comparing only first elements for PoC
+    assert all([a[0] == b[0] for a,b in zip(ret_val,expected_val)])
 
-    matrix = csr_matrix(values).toarray()
-    print(matrix)
-    
+def test_numpy():
+    expected_val =  [0., 0., 0., 0., 0.]
+    ret_val = np.zeros(5)
+    assert all([a == b for a,b in zip(ret_val,expected_val)])
+
+def test_pandas():
+    ages = pd.Series([22, 35, 58], name="Age")
+    ret_val = ages.max()
+
+    assert ret_val == 58
